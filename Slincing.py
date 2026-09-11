f23a325572e6a3db15604eed35a55aa76389ccde
GARIDI YASWANTH <garidiyaswanth2002@gmail.com>
2026-09-11 00:18:16 -0800

    Add files via upload

---
+++ Slincing.py
@@ -0,0 +1,40 @@
+# numbers = [10, 20, 30, 40, 50]
+
+# print(numbers[1:4])
+
+# text = "Python"
+
+# print(text[0:3])
+
+
+# text = "Python"
+
+# text[:3]    
+# text[2:]     
+# text[::2]    
+# text[::-1]   
+
+# print(text[:3])      #  'Pyt'    → beginning to index 3
+# text[2:]             #'thon'   → index 2 to the end
+# text[::2]            # 'Pto'    → every second character
+# text[::-1]           # 'nohtyP' → reverse the string
+# print(text[2:])
+# print(text[::2])
+# print(text[::-1])
+
+
+# print(text[::1])
+
+# print(text[::3])
+
+# print(text[::5])
+
+# numbers = [10, 20, 30, 40, 50]
+# print(numbers[1:4])
+
+
+# name = "Python"
+# print(name[0:3])
+
+# numbers = [1, 2, 3, 4, 5, 6, 7]
+# print(numbers[5:1:-1])
